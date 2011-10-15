2bf33351521e0c056139a5424841bf3a196a4b39
Stefan Kanev <stefan.kanev@gmail.com>
2011-10-16 00:25:04 +0300

    Reorganize ruby code

--- build.rb
+++ build.rb
@@ -1,17 +1,4 @@
-require 'rubygems'
-require 'slim'
-require 'yaml'
-require 'fileutils'
-
-require './lib/example'
-require './lib/slide_helper'
-require './lib/presentation'
-require './lib/builder'
-
-Slim::Engine.default_options[:disable_escape] = true
-Slim::Engine.default_options[:pretty] = true
-
-Slim::EmbeddedEngine.register :example, Example
+require './lib/slides'
 
 FileUtils.rm_rf 'target'
 FileUtils.mkdir_p 'target'
---
+++ lib/slides.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'slim'
+require 'yaml'
+require 'fileutils'
+
+$:.unshift File.dirname(__FILE__) + '/slides'
+
+autoload :Example,      'example'
+autoload :SlideHelper,  'slide_helper'
+autoload :Presentation, 'presentation'
+autoload :Builder,      'builder'
+
+Slim::Engine.default_options[:disable_escape] = true
+Slim::Engine.default_options[:pretty] = true
+
+Slim::EmbeddedEngine.register :example, Example
