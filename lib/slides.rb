9b347d087dea172da0f8fd95f6152f08fc7ce576
Stefan Kanev <stefan.kanev@gmail.com>
2011-10-16 13:25:49 +0300

    Don't use pretty slim
    Messes up empty lines in pre blocks

--- lib/slides.rb
+++ lib/slides.rb
@@ -11,6 +11,5 @@ autoload :Lecture,     'lecture'
 autoload :Builder,     'builder'
 
 Slim::Engine.default_options[:disable_escape] = true
-Slim::Engine.default_options[:pretty] = true
 
 Slim::EmbeddedEngine.register :example, Example
